ff2f062d4b1228caf2742f3ecc387f1108bba048
Angie Ortiz <angienikoloa@ufps.edu.co>
2025-10-14 18:45:27 -0500

    triqui

---
+++ src/app/triqui/page.js
@@ -0,0 +1,126 @@
+"use client";
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+    return (
+        <button onClick={onSquareClick}
+            className="w-24 h-24 bg-white rounded-lg border-4 border-rose-100 
+             flex items-center justify-center text-3xl font-bold text-pink-500 
+             shadow-md hover:bg-indigo-100 transition">
+            {value}
+        </button>
+    );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+        onPlay(nextSquares);
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else if (!squares.includes(null)) {
+        status = 'Empate';
+    }
+    else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    return (
+        <>
+            <div className="status">{status}</div>
+            <div className='space-y-3 bg-rose-300 p-5 rounded-xl shadow-lg'>
+                <div className="grid grid-cols-3 gap-3">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+                </div>
+                <div className="grid grid-cols-3 gap-3">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+                </div>
+                <div className="grid grid-cols-3 gap-3 ">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button className='m-1 px-3 flex justify-items-center bg-white rounded-lg border-2 border-rose-100' onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen
+                bg-gradient-to-br from-purple-200 to-rose-200 p-6">
+            <h1 className='font-bold text-2xl m-2'>JUEGO TIC TAC TOE</h1>
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
